447b5025155df298d5be69d29c768752fab3880e
Ognjen Mamic <Ognjen.mamic@evidanza.de>
2024-08-06 09:06:49 +0200

    Revert "Delete Document from Table"
    This reverts commit 69d90fee62f53f99e5590b504ef0aa08dd7ceb45. und Dropdown Funktion bei Agent Page

--- app/api/knowledge/route.ts
+++ app/api/knowledge/route.ts
@@ -1,31 +1,34 @@
 import { NextResponse } from "next/server";
-<<<<<<< HEAD
 import { PrismaClient } from "@prisma/client";
-=======
-import { PrismaClient, Prisma } from "@prisma/client";
->>>>>>> parent of 69d90fe (Delete Document from Table)
+
+const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   try {
-    const body = await req.json();
-    console.log(body);
-    const prisma = new PrismaClient();
-    const document = await prisma.document.create({
-      data: {
-        name: body.name,
-        description: body.description,
-        content: body.anweisungen,
-      },
-    });
-    const { messages } = body;
-    console.log("==========");
-    console.log(messages);
+    // Proveri da li je zahtev multipart/form-data
+    const contentType = req.headers.get("content-type");
+    if (contentType && contentType.includes("multipart/form-data")) {
+      const formData = await req.formData();
+      const name = formData.get("name") as string;
+      const description = formData.get("description") as string;
+      const anweisungen = formData.get("anweisungen") as string;
 
-    if (!messages) {
-      return new NextResponse("Messages are required", { status: 400 });
+      if (!name || !description || !anweisungen) {
+        return new NextResponse("Invalid input", { status: 400 });
+      }
+
+      const document = await prisma.document.create({
+        data: {
+          name,
+          description,
+          content: anweisungen,
+        },
+      });
+
+      return new NextResponse(JSON.stringify(document), { status: 200 });
     }
 
-    return new NextResponse(JSON.stringify({}), { status: 200 });
+    return new NextResponse("Unsupported Media Type", { status: 415 });
   } catch (error) {
     console.log("[CODE_ERROR]", error);
     return new NextResponse("Internal error", { status: 500 });
@@ -33,8 +36,23 @@ export async function POST(req: Request) {
 }
 
 export async function GET(req: Request) {
-  const prisma = new PrismaClient();
   const documents = await prisma.document.findMany();
   return new NextResponse(JSON.stringify(documents), { status: 200 });
 }
 
+export async function DELETE(req: Request) {
+  try {
+    const { id } = await req.json();
+
+    const deletedDocument = await prisma.document.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    return new NextResponse(JSON.stringify(deletedDocument), { status: 200 });
+  } catch (error) {
+    console.log("[DELETE_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
