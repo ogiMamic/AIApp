a763c9ed051d6f53c85d541295770628f87e3631
Stan Vranic <stanislav.vranic@evidanza.de>
2024-11-27 09:51:56 +0100

    feat: add sidebar and use-mobile

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ const DashboardLayout = ({
     children: React.ReactNode;
 }) => {
     return(
-        <div className="h-full relative">
+        <div className="flex-1 h-full relative">
             <div className="hidden h-full md:flex md:w-72
             md:flex-col md:fixed md:inset-y-0 z-[80]
             bg-gray-900">
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
-        <Toaster />
+        <body className={inter.className}>
+          <SidebarProvider>{children}<Toaster /></SidebarProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
